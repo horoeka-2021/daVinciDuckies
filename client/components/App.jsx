914a54c3fcc82ad0d5732b3a71cbfe71926f9694
Julianne McCoun <jmccoun7@gmail.com>
2021-11-12 10:42:27 +1300

    Uncommented the useEffect function and imported getPrice function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,27 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import Home from './Home'
 
+import { getPrice } from '../api'
 
 function App () {
-  const[art, setArt] = useState([])
-  
-// Test to make sure getPrice works as intended
-  
-  // useEffect(() => {
-  //   getPrice()
-  //     .then(price => {
-  //       console.log('This is the price', price)
-  //       return null
-  //     })
-  //     .catch(err => console.error('Oops! You fucked up', err.message))
-  // }, [])
+  // const [art, setArt] = useState([])
 
-
+  // Test to make sure getPrice works as intended
+  useEffect(() => {
+    getPrice()
+      .then(price => {
+        console.log('This is the price', price)
+        return null
+      })
+      .catch(err => console.error('Oops! You fucked up', err.message))
+  }, [])
 
   return (
     <div>
-       <Home />
-       
+      <Home />
     </div>
   )
 }
