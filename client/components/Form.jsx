a9356f2ba69da10c28639d162d004277d2a3e134
joshuawc <joshkosh99@gmail.com>
2021-11-12 11:19:42 +1300

    Wire up post route

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,37 +1,47 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react'
+import { postGuess } from '../api'
 
-function guessingForm(props){
-    const[input, setInput] = useState(0)
+function guessingForm (props) {
+  const [input, setInput] = useState(0)
 
-    function handleChange(event){
-        setInput(event.target.value)
-    }
-   
-    function handleAdd(event){
+  function handleChange (event) {
+    setInput(event.target.value)
+  }
+  
+  function handleAdd (event) {
     event.preventDefault()
-
     
-
+    if (isNaN(input)) {
+      setInput(0)
+      console.log('write a number dork')
+      return
     }
+    postGuess(Number(input))
+      .then(response => {
+        console.log(response)
+        setInput(0)
+        return null
+      })
+    .catch(err => console.error('You can\'t even submit a form! ', err.message))
+  }
 
-return (
-   <>
-        
-    <form action="">
-      <fieldset>
-        <legend>Make A guess</legend>
-        <label>
-          <span>Price</span>
-          <input name="price" value={input} onChange={handleChange}/>
-        </label> <br />
+  return (
+    <>
 
-        <button onClick={handleAdd}>Guess</button>
+      <form action="POST">
+        <fieldset>
+          <legend>Make A guess</legend>
+          <label>
+            <span>Price</span>
+            <input name="price" value={input} onChange={handleChange}/>
+          </label> <br />
 
-      </fieldset>
-    </form>
-    </>  
-)
-   
+          <button onClick={handleAdd}>Guess</button>
+
+        </fieldset>
+      </form>
+    </>
+  )
 }
 
 export default guessingForm
