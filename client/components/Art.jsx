643cc7fb2e5175d65031abfd72402ee574109772
Julianne McCoun <jmccoun7@gmail.com>
2021-11-12 11:17:59 +1300

    Added a max width to the duck images

--- client/components/Art.jsx
+++ client/components/Art.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getDuck } from '../api'
 
-function Art(props){
-    console.log(props)
-return (
-   <>
-   <div className="art-container">
+function Art () {
+  const [duck, setDuck] = useState('')
+  useEffect(() => {
+    getDuck()
+      .then(result => {
+        const newResult = result.output
+        setDuck(newResult)
+        console.log(newResult)
+        console.log(duck)
+        return null
+      })
+      .catch(err => console.error('Oops! There was an error: ', err))
+  }, [])
 
-  
-        {/* <h1>{props.data.title}</h1>
+  return (
+    <>
+      <div className="art-container">
+        <img className="duck-Image" src={duck} />
+        { /* <h1>{props.data.title}</h1>
         <img className="duck-Image" src={props.data.image} />
-        <p>{props.data.description}</p> */}
-    </div>
-    </>  
-)
-   
+        <p>{props.data.description}</p> */ }
+      </div>
+    </>
+  )
 }
 
 export default Art
