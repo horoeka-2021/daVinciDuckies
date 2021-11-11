be6b09b5db00d91f2ca8cc86617702bfe155585e
joshuawc <joshkosh99@gmail.com>
2021-11-12 10:27:27 +1300

    getPrice working

--- client/api.js
+++ client/api.js
@@ -2,8 +2,21 @@ import request from 'superagent'
 
 const homeUrl = '/api/v1/home/'
 
-export function getData () {
+function randomIndex(arr) {
+  return Math.floor(Math.random() * arr.length)
+}
+
+export function getDuck () {
   return request
     .get(homeUrl)
     .then(response => response.body)
+}
+
+export function getPrice() {
+  return request
+    .get('https://www.cheapshark.com/api/1.0/deals')
+    .then(response => {
+      const index = randomIndex(response.body)
+      return response.body[index].salePrice
+    })
 }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getPrice } from '../api'
 
-function App () {
+function App() {
+  
+
+  useEffect(() => {
+    getPrice()
+      .then(price => {
+        console.log('This is the price', price)
+        return null
+      })
+      .catch(err => console.error('Oops! You fucked up', err.message))
+  }, [])
   return (
     <div>
       <h1>da Vinci Duckies</h1>
