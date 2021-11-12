2b05b0dc58d552fb17f105d26a4433d1502d61f6
ppc99999 <ppc99999@gmail.com>
2021-11-12 15:02:06 +1300

    heading

--- client/components/Art.jsx
+++ client/components/Art.jsx
@@ -28,12 +28,6 @@ function Art ({ toggle, setToggle, display, setDisplay }) {
 
   return (
     <section className="art-section">
-      <div className='directions' >
-        <p>
-          Directions: Look at the quacky artwork to the left - how much do you think it is worth?
-          Type your answer below the duck, if you are within 10% of the real price, you get a point! You can see your points at the top of the page. When you get to 1 point, you win!
-        </p>
-      </div>
 
       <div className="art-container frame">
         <img className="duck-Image" src={duck} />
